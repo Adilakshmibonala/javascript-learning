5ddcabb5654da81ac0449b842a8a2c43261c7055
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-03-19 20:38:03 +0530

    added heading manipulation files

---
+++ js_files/heading_manipulation.js
@@ -0,0 +1,5 @@
+function manipulateStyles(){
+    let document_object = document.getElementById("headingElement")
+    document_object.textContent = "4.0 Technologies"
+    document_object.style.color = "blue"
+}
