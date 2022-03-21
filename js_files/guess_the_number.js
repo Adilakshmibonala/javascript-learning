5989c3af85b386dd63c64e84d74222a17b1cc5b5
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-03-21 09:05:46 +0530

    Add guess the number practice files

---
+++ js_files/guess_the_number.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.ceil(Math.random() * 100)
+
+function checkGuessedNumber() {
+    let guessedNumber = parseInt(document.getElementById("guessedNumber").value)
+    let responseMessageElement = document.getElementById("gameResult")
+    if (guessedNumber > randomNumber) {
+        responseMessageElement.textContent = "Too High! Try Again"
+        responseMessageElement.style.backgroundColor = "blue"
+    }
+    else if (guessedNumber < randomNumber) {
+        responseMessageElement.textContent = "Too Low! Try Again"
+        responseMessageElement.style.backgroundColor = "blue"
+    }
+    else if (guessedNumber === randomNumber) {
+        responseMessageElement.textContent = "Congratulations! You Got It Right"
+        responseMessageElement.style.backgroundColor = "green"
+    }
+}
