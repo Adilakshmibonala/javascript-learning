581edb564a8b5fd635f63cc8fe56fdf8e48dc111
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-04-02 17:24:43 +0530

    Minor changes in color picker

--- js_files/color_picker.js
+++ js_files/color_picker.js
@@ -5,9 +5,6 @@ function cementColor() {
     paragraphElement.textContent = "Background Color: #e0e0e0"
     divElement.style.backgroundColor = "#e0e0e0"
     document.getElementById("cementColor").style.backgroundColor = "#e0e0e0"
-    document.getElementById("greenColor").style.backgroundColor = "#ffffff"
-    document.getElementById("blueColor").style.backgroundColor = "#ffffff"
-    document.getElementById("roseColor").style.backgroundColor = "#ffffff"
 }
 
 
@@ -15,9 +12,6 @@ function greenColor() {
     paragraphElement.textContent = "Background Color: #6fcf97"
     divElement.style.backgroundColor = "#6fcf97"
     document.getElementById("greenColor").style.backgroundColor = "#6fcf97"
-    document.getElementById("cementColor").style.backgroundColor = "#ffffff"
-    document.getElementById("blueColor").style.backgroundColor = "#ffffff"
-    document.getElementById("roseColor").style.backgroundColor = "#ffffff"
 }
 
 
@@ -25,17 +19,10 @@ function blueColor() {
     paragraphElement.textContent = "Background Color: #56ccf2"
     divElement.style.backgroundColor = "#56ccf2"
     document.getElementById("blueColor").style.backgroundColor = "#56ccf2"
-    document.getElementById("greenColor").style.backgroundColor = "#ffffff"
-    document.getElementById("cementColor").style.backgroundColor = "#ffffff"
-    document.getElementById("roseColor").style.backgroundColor = "#ffffff"
 }
 
 
 function roseColor() {
     paragraphElement.textContent = "Background Color: #bb6bd9"
     divElement.style.backgroundColor = "#bb6bd9"
-    document.getElementById("roseColor").style.backgroundColor = "#bb6bd9"
-    document.getElementById("greenColor").style.backgroundColor = "#ffffff"
-    document.getElementById("cementColor").style.backgroundColor = "#ffffff"
-    document.getElementById("blueColor").style.backgroundColor = "#ffffff"
 }
